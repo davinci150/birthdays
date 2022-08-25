41edcac863563ba0fcb278f02f4b2d8ccffb5873
m0rt95 <i.am.nikola7@gmail.com>
2022-08-25 14:12:36 +0300

    feat: adding page user profile

---
+++ lib/profile/user_profile_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '../contacts_repository.dart';
+import '../model/user_model.dart';
+
+class UserProfilePage extends StatefulWidget {
+  const UserProfilePage({Key? key,
+    required this.id,
+  }) : super(key: key);
+  final int id;
+
+  @override
+  State<UserProfilePage> createState() => _UserProfilPageState();
+}
+
+class _UserProfilPageState extends State<UserProfilePage> {
+  late ContactsRepository repository;
+  late UserModel user;
+  @override
+  void initState() {
+    repository = ContactsRepository.instance;
+    //user = repository.getById(widget.id);
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
