3948c6d6872207b18a7e75831788a7774532d240
m0rt95 <i.am.nikola7@gmail.com>
2022-08-30 15:02:18 +0300

    refactor: change avatar

--- lib/widgets/avatar.dart
+++ lib/widgets/avatar.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 
+import '../home/widgets/user_avatar_wdget.dart';
 import '../model/user_model.dart';
 
 class CustomAvatar extends StatelessWidget {
   const CustomAvatar({Key? key,
-    this.userModel,
+    required this.userModel,
     this.radius = 60
   }) : super(key: key);
-  final UserModel? userModel;
+  final UserModel userModel;
   final double radius;
 
   @override
   Widget build(BuildContext context) {
-    final listImage =
-    List.generate(12, (index) => 'assets/images/human$index.png');
     return Stack(
       alignment: Alignment.topRight,
       children: [
@@ -25,30 +24,11 @@ class CustomAvatar extends StatelessWidget {
                 spreadRadius: 3,
                 color: Color.fromRGBO(0, 0, 0, 0.1))
           ], color: Colors.white, shape: BoxShape.circle),
-          child: userModel?.avatar?.isNotEmpty ?? false
+          child: userModel.avatar?.isNotEmpty ?? false
               ? CircleAvatar(
-              backgroundImage: MemoryImage(userModel!.avatar!),
-              radius: 60)
-              : Stack(
-            alignment: Alignment.center,
-            children: [
-              CircleAvatar(
-                radius: radius,
-                backgroundColor: stringToHslColor(userModel!.name!, 0.4, 0.7),
-              ),
-              Container(
-                padding: const EdgeInsets.only(bottom: 10),
-                child: Image.asset(
-                  listImage[avatarFromName(userModel!.name!, listImage.length)],
-                  height: radius * 2 - 5,
-                ),
-              )
-            ],
-          ),
-          // CircleAvatar(
-          //     radius: 60,
-          //     child: Text(userModel!.initials().toUpperCase(),
-          //         style: const TextStyle(fontSize: 34))),
+              backgroundImage: MemoryImage(userModel.avatar!),
+              radius: radius)
+              : UserAvatar(user: userModel, radius: radius,),
         ),
         GestureDetector(
             onTap: () {
@@ -61,29 +41,5 @@ class CustomAvatar extends StatelessWidget {
                 child: const Icon(Icons.camera_alt_outlined)))
       ],
     );
-  }
-
-  Color stringToHslColor(String str, double saturation, double lightness) {
-    int hash = 0;
-
-    for (int i = 0; i < str.length; i++) {
-      hash = str.codeUnitAt(i) + ((hash << 5) - hash);
-    }
-
-    final int hue = hash % 360;
-
-    //alpha 0.0-1.0, hue 0.0-360.0,  saturation 0.0-1.0,  lightness 0.0-1.0
-    return HSLColor.fromAHSL(1.0, hue.toDouble(), saturation, lightness)
-        .toColor();
-  }
-
-  int avatarFromName(String str, int lenght) {
-    int hash = 0;
-
-    for (int i = 0; i < str.length; i++) {
-      hash = str.codeUnitAt(i) + ((hash << 5) - hash);
-    }
-
-    return hash % lenght;
   }
 }
