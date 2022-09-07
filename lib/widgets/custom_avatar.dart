32ae6fe47d83b0bddf631cd8e771a1c2734cf11f
m0rt95 <i.am.nikola7@gmail.com>
2022-09-07 13:27:11 +0300

    feat: add choose image picker

--- lib/widgets/custom_avatar.dart
+++ lib/widgets/custom_avatar.dart
@@ -1,15 +1,36 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../home/widgets/user_avatar_wdget.dart';
 import '../model/user_model.dart';
 
-class CustomAvatar extends StatelessWidget {
-  const CustomAvatar({Key? key,
-    required this.userModel,
-    this.radius = 60
-  }) : super(key: key);
+class CustomAvatar extends StatefulWidget {
+  const CustomAvatar({Key? key, required this.userModel, this.radius = 60,
+  })
+      : super(key: key);
   final UserModel userModel;
   final double radius;
+  @override
+  State<CustomAvatar> createState() => _CustomAvatarState();
+}
+
+class _CustomAvatarState extends State<CustomAvatar> {
+   File? image;
+
+  Future<void> pickImage() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (image == null) return;
+
+      final imageTemporary = File(image.path);
+      setState(() => this.image = imageTemporary);
+    } on PlatformException catch(e){
+      print('Failed to pick image $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,12 +45,24 @@ class CustomAvatar extends StatelessWidget {
                 spreadRadius: 3,
                 color: Color.fromRGBO(0, 0, 0, 0.1))
           ], color: Colors.white, shape: BoxShape.circle),
-          child: UserAvatar(user: userModel, radius: radius,),
+          child:
+              ClipOval(
+              child: image != null
+                  ? Image.file(
+                image!,
+                width: widget.radius *2,
+                height: widget.radius *2,
+                fit: BoxFit.cover,
+              )
+                  : UserAvatar(
+                user: widget.userModel,
+                radius: widget.radius,
+              ),
+              )
+
         ),
         GestureDetector(
-            onTap: () {
-              print('camera');
-            },
+            onTap: pickImage,
             child: Container(
                 padding: const EdgeInsets.all(7),
                 decoration: const BoxDecoration(
